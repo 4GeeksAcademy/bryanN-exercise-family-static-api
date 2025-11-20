8647d141a89b76409b8460a5abf77cdc5641df59
Bryan Navarro <diazbnavarro@gmail.com>
2025-11-20 03:57:24 +0000

    finishing data structures

--- src/datastructures.py
+++ src/datastructures.py
@@ -25,20 +25,37 @@ class FamilyStructure:
         self._next_id += 1
         return generated_id
 
-    def add_member(self, member):
+    def add_member(self, first_name, age, lucky_numbers):
+        self.age = age
+        self.lucky_numbers = lucky_numbers
         ## You have to implement this method
+        new_member = {
+            'id': self._generate_id(),
+            'first_name': first_name,
+            'last_name': self.last_name,
+            'age': age,
+            'lucky_numbers': lucky_numbers
+        }
         ## Append the member to the list of _members
-        pass
-
+        self._members.append(new_member)
+        return new_member
+     
     def delete_member(self, id):
         ## You have to implement this method
+        for item in self._members:
+            if item['id'] == id:
+                self._members.remove(item)
+                return item
         ## Loop the list and delete the member with the given id
-        pass
+        return None
 
     def get_member(self, id):
         ## You have to implement this method
         ## Loop all the members and return the one with the given id
-        pass
+        for item in self._members:
+            if item['id'] == id:
+                return item
+        return None
 
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
